267e5e003f35968deb6e55e8ec5c349867866f49
ywpDerek <624181287@qq.com>
2017-05-04 17:15:58 +0800

    网上商城

--- js/shop_product.js
+++ js/shop_product.js
@@ -50,4 +50,80 @@ var oRight = getClass('right')[0];
 		if(target != oUp && target != oLeft && target != oRight){
 			oUl.style.display = 'none';
 		}
-	}	
+	}
+
+//弹层
+var oSmallImg = getClass('small-img')[0];
+var aDialogLi = oSmallImg.getElementsByTagName('li');
+var oDialogBox = getClass('dialog-box')[0];
+var oDialogBody = getClass('dialog-body')[0];
+var oDialogImg = oDialogBody.getElementsByTagName('img')[0];
+var oDialogClose = getClass('dialog-close',oDialogBox)[0];
+var oDialogPrev = getClass('dialog-prev',oDialogBox)[0];
+var oDialogNext = getClass('dialog-next',oDialogBox)[0];
+var oSlideBtn = getClass('slide-btn',oDialogBox)[0];
+var oContent = getClass('content',oDialogBox)[0];
+var iNow = 0;
+for(var i=0; i<aDialogLi.length; i++){
+	aDialogLi[i].index = i;
+	aDialogLi[i].onclick = function(){
+		var oImg = this.getElementsByTagName('img')[0];
+		oDialogBox.style.display = 'block';
+		oDialogImg.src = oImg.src;
+
+		oContent.style.animation = 'show 1s ease forwards';
+		iNow = this.index;
+	}
+}
+//关闭弹曾
+oDialogBox.onclick = function(e){
+	var target = e.target || event.srcElement;
+	//事件源就是点哪个div身上就会在谁身上发生  不会发生冒泡现象
+	if(target == oDialogBox || target == oDialogClose){
+		oDialogBox.style.display = 'none';
+	}
+}
+oDialogPrev.onclick = function(){
+	iNow--;
+	if(iNow == -1){
+		iNow = aDialogLi.length-1;
+	}
+	slideNext();
+	// oDialogImg.src = prevLi.getElementsByTagName('img')[0].src;
+}
+oDialogNext.onclick = function(){
+	iNow++;
+	iNow = iNow%aDialogLi.length;
+	slideNext();
+	// oDialogImg.src = prevLi.getElementsByTagName('img')[0].src;
+}
+function slideNext(){
+	//取到下一个li
+	var nextLi = aDialogLi[iNow];
+	//取到下一个li里的图片的src属性
+	var nextSrc = nextLi.getElementsByTagName('img')[0].src;
+	//创建一个新的img
+	var oImg = document.createElement('img');
+	oImg.src = nextSrc;
+	//获取到原有图片
+	var oldImg = oDialogBody.children[0];
+	//插入一张新图，在原有图片前边
+	oDialogBody.insertBefore(oImg,oldImg);
+	//原有图片动画隐藏
+	oldImg.style.animation = 'hide 1s ease forwards';
+	//原有图片动画隐藏后 remove掉
+	setTimeout(function(){
+		oDialogBody.removeChild(oldImg);
+	},1000)
+}
+var timer = '';
+oSlideBtn.onclick = function(){
+	if(timer){
+		clearInterval(timer);
+		timer = '';
+	}else{
+		timer = setInterval(function(){
+			oDialogNext.onclick();
+		},1000)
+	}
+}
