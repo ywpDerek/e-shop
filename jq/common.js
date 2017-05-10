5e40377522d8665b9408493bc65b10533d95af8e
ywpDerek <624181287@qq.com>
2017-05-10 19:19:18 +0800

    练习

---
+++ jq/common.js
@@ -0,0 +1,5 @@
+$('.menu-btn').hover(function(){
+	$('.menu-btn').find('.sub-menu').show().animate({
+		top:56
+	});
+});
--- jq/shop_product.js
+++ jq/shop_product.js
@@ -20,4 +20,42 @@ $(document).on('click',function(e){
 $('.btn-box li').on('click',function(){
 	$(this).addClass('active').siblings().removeClass('active');
 	$('.tab-item').eq($(this).index()).show().siblings().hide();
+});
+//弹层
+var iNow = 0;
+$('.small-img li').on('click',function(){
+	$('.dialog-box').show();
+	$('.content').animate({
+		top:'50%'
+	},1000);
+	$('.dialog-body').find('img').attr('src',$(this).find('img').attr('src'));
+	// console.log($('.dialog-body').find('img').attr('scr'))
+	iNow = $('.small-img li').index();
+});
+$('.dialog-box').on('click',function(e){
+	if(e.target == $('.dialog-close')[0] || e.target == $('.dialog-box')[0]){
+		$('.dialog-box').hide();
+		$('.content').stop().css('top','-249px');
+	}
+});
+$('.dialog-next').on('click',function(){
+	iNow++;
+	iNow = iNow%$('.small-img li').length;
+
+	var $NextImgSrc = $('.small-img img').eq(iNow).attr('src');
+	$('.dialog-body img').eq(0).before('<img src="'+$NextImgSrc+'"/>');
+	$('.dialog-body img').eq(0).siblings('img').fadeOut(1000,function(){
+		$(this).remove();
+	});
+});
+$('.dialog-prev').on('click',function(){
+	iNow--;
+	if(iNow == -1){
+		iNow = $('.small-img li').length-1;
+	}
+	var $PrevImgSrc = $('.small-img img').eq(iNow).attr('src');
+	$('.dialog-body img').eq(0).before('<img src="'+$PrevImgSrc+'"/>');
+	$('.dialog-body img').eq(0).siblings('img').fadeOut(1000,function(){
+		$(this).remove();
+	});
 })
